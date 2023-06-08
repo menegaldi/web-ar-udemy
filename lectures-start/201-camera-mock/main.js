02b363cd30b254d3db20020737ee998f22528a88
Larissa Poderoso <l.menegaldi@gmail.com>
2023-06-08 13:35:54 +0200

    Use camera mock

--- lectures-start/201-camera-mock/main.js
+++ lectures-start/201-camera-mock/main.js
@@ -1,15 +1,33 @@
 const THREE = window.MINDAR.IMAGE.THREE;
+import { mockWithVideo, mockWithImage } from '../../libs/camera-mock.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const start = async() => {
+  const start = async () => {
+
+    //mockWithVideo("../../assets/mock-videos/course-banner1.mp4")
+    mockWithImage("../../assets/mock-videos/course-banner1.png")
+
+    // navigator.mediaDevices.getUserMedia = () => {
+    //   return new Promise((resolve, reject) => {
+    //     const video = document.createElement("video");
+    //     video.setAttribute("src", "../../assets/mock-videos/course-banner1.mp4");
+    //     video.setAttribute("loop", "");
+
+    //     video.oncanplay = () => {
+    //       video.play();
+    //       resolve(video.captureStream());
+    //     }
+    //   })
+    // }
+
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
       imageTargetSrc: '../../assets/targets/course-banner.mind',
     });
-    const {renderer, scene, camera} = mindarThree;
+    const { renderer, scene, camera } = mindarThree;
 
     const geometry = new THREE.PlaneGeometry(1, 1);
-    const material = new THREE.MeshBasicMaterial({color: 0x00ffff, transparent: true, opacity: 0.5});
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ffff, transparent: true, opacity: 0.5 });
     const plane = new THREE.Mesh(geometry, material);
 
     const anchor = mindarThree.addAnchor(0);
