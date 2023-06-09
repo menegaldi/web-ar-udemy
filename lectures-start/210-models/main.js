63881eed8512f69eab1c1e70cb67fa0848d94383
Larissa Poderoso <l.menegaldi@gmail.com>
2023-06-09 13:27:53 +0200

    Import 3D models and create a scene

--- lectures-start/210-models/main.js
+++ lectures-start/210-models/main.js
@@ -1,14 +1,33 @@
+import { GLTFLoader } from "../../libs/three.js-r132/examples/jsm/loaders/GLTFLoader.js"
 const THREE = window.MINDAR.IMAGE.THREE;
+import { loadGLTF } from "../../libs/loader.js";
+
+// const loadGLTF = (path) => {
+//   return new Promise((resolve, reject) => {
+//     const loader = new GLTFLoader();
+//     loader.load(path, (gltf) => {
+//       resolve(gltf);
+//     })
+//   })
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const start = async() => {
+  const start = async () => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
       imageTargetSrc: '../../assets/targets/musicband.mind',
     });
-    const {renderer, scene, camera} = mindarThree;
+    const { renderer, scene, camera } = mindarThree;
+
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    scene.add(light)
 
     const anchor = mindarThree.addAnchor(0);
+
+    const gltf = await loadGLTF("../../assets/models/musicband-raccoon/scene.gltf");
+    gltf.scene.scale.set(0.1, 0.1, 0.1);
+    gltf.scene.position.set(0, -0.4, 0);
+    anchor.group.add(gltf.scene);
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
