fc66bf48266a734b855f9129be6c84ffe8e92d86
Larissa Poderoso <l.menegaldi@gmail.com>
2023-06-13 22:35:52 +0200

    Added animations to 3D models

--- lectures-start/212-multi-track/main.js
+++ lectures-start/212-multi-track/main.js
@@ -1,15 +1,16 @@
-import {loadGLTF} from "../../libs/loader.js";
+import { loadGLTF } from "../../libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const start = async() => {
+  const start = async () => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
       imageTargetSrc: '../../assets/targets/musicband.mind',
+      maxTrack: 2,
     });
-    const {renderer, scene, camera} = mindarThree;
+    const { renderer, scene, camera } = mindarThree;
 
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     scene.add(light);
 
     const raccoon = await loadGLTF("../../assets/models/musicband-raccoon/scene.gltf");
--- lectures-start/213-animations/main.js
+++ lectures-start/213-animations/main.js
@@ -1,15 +1,15 @@
-import {loadGLTF} from "../../libs/loader.js";
+import { loadGLTF } from "../../libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const start = async() => {
+  const start = async () => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
       imageTargetSrc: '../../assets/targets/musicband.mind',
     });
-    const {renderer, scene, camera} = mindarThree;
+    const { renderer, scene, camera } = mindarThree;
 
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     scene.add(light);
 
     const gltf = await loadGLTF('../../assets/models/musicband-raccoon/scene.gltf');
@@ -19,8 +19,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const anchor = mindarThree.addAnchor(0);
     anchor.group.add(gltf.scene);
 
+    // gltf.animations
+    const mixer = new THREE.AnimationMixer(gltf.scene);
+    const action = mixer.clipAction(gltf.animations[0]);
+    action.play();
+
+    const clock = new THREE.Clock();
+
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
+      const delta = clock.getDelta();
+
+
+      gltf.scene.rotation.set(0, gltf.scene.rotation.y + delta, 0);
+
+      mixer.update(delta);
       renderer.render(scene, camera);
     });
   }
