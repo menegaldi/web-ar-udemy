6ea1a58076fb369349e75f4e74b5bbac2c2f2784
Larissa Poderoso <l.menegaldi@gmail.com>
2023-06-14 22:42:14 +0200

    Added events to play audios

--- lectures-start/220-events/main.js
+++ lectures-start/220-events/main.js
@@ -1,15 +1,15 @@
-import {loadGLTF} from "../../libs/loader.js";
+import { loadGLTF } from "../../libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const start = async() => {
+  const start = async () => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
       imageTargetSrc: '../../assets/targets/musicband.mind',
     });
-    const {renderer, scene, camera} = mindarThree;
+    const { renderer, scene, camera } = mindarThree;
 
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     scene.add(light);
 
     const raccoon = await loadGLTF('../../assets/models/musicband-raccoon/scene.gltf');
@@ -18,6 +18,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const anchor = mindarThree.addAnchor(0);
     anchor.group.add(raccoon.scene);
+
+    anchor.onTargetFound = () => {
+      console.log("on target found")
+    }
+
+    anchor.onTargetLost = () => {
+      console.log("on target lost")
+    }
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
